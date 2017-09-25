8b6b67404f7fce4ed8907624a330a1cd21ab4dbb
chary344 <poczta.szymona@gmail.com>
2017-09-25 20:52:18 +0200

    Initial commit

---
+++ tests/pages/__init__.py
@@ -0,0 +1,5 @@
+"""Put your PageObjects here."""
+
+# Import your subpages Implementing an Interface in the
+# "__init__.py" so PageFactory will no about it's existence.
+from tests.pages.insysgo_pages import MyListPage
